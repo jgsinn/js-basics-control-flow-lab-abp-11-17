6bafc428e75dd87d5125d28de8514b1c23228672
Justin Sinn <justin.sinn.co@gmail.com>
2017-11-15 05:44:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function ternaryCheckCity(city){
 
 function switchOnCharmFromTip(tip){
   switch (new Date().getDay()) {
-    case "generous":
+    case 'generous':
         return 'Thank you so much.'
     case 1:
         return 'Thank you.'
