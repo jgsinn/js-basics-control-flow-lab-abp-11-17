6b985babed09013014d54d05b6d8f3a9b5a7e6be
Justin Sinn <justin.sinn.co@gmail.com>
2017-11-15 05:47:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function switchOnCharmFromTip(tip){
   switch (tip) {
     case 'generous':
         return 'Thank you so much.'
-    case 'not so generous':
+    case 'not as generous':
         return 'Thank you.'
     default:
         return 'Bye.'
